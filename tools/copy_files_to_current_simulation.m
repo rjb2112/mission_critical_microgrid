17c271f4eb396fb727a8b887b438867359760c2c
rjb2112 <robert.james.baker.2112@gmail.com>
2023-05-31 13:52:33 -0700

    Update copy_files_to_current_simulation.m

--- tools/copy_files_to_current_simulation.m
+++ tools/copy_files_to_current_simulation.m
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 % copy files to current simulation
 
 function copy_files_to_current_simulation(use_case_id, alternative_id)
@@ -11,22 +10,4 @@ directory_destination = strcat("current_simulation");
 file_destination = "x_T_1s.csv" ;
 filename_destination = strcat(directory_destination,"/",file_destination) ;
 
-copyfile(filename_source, filename_destination)
-
-=======
-% copy files to current simulation
-
-function copy_files_to_current_simulation(use_case_id, alternative_id)
-
-directory_source = strcat("use_cases/use_case_",use_case_id,"/",alternative_id);
-file_source = "x_T_1s.csv" ;
-filename_source = strcat(directory_source,"/",file_source) ;
-
-directory_destination = strcat("current_simulation");
-file_destination = "x_T_1s.csv" ;
-filename_destination = strcat(directory_destination,"/",file_destination) ;
-
-copyfile(filename_source, filename_destination)
-
->>>>>>> 3bbaaf5c3d6728a14d9f6112b2c18ed6a730e9e9
-end
+copyfile(filename_source, filename_destination) ;
