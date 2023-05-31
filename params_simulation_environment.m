0084528b22bffa0d3b82516c76ef963fd37d3960
rjb2112 <robert.james.baker.2112@gmail.com>
2023-05-31 13:12:06 -0700

    Update params_simulation_environment.m

--- params_simulation_environment.m
+++ params_simulation_environment.m
@@ -4,20 +4,20 @@ cd tools
 
 % ---------- DATA WRANGLING
 
-handle_build_source_datasets = @build_source_datasets
-handle_interpolate_solar     = @interpolate_solar
+handle_build_source_datasets = @build_source_datasets   ;
+handle_interpolate_solar     = @interpolate_solar       ;
 
 
 % ---------- USE CASE, TIME HORIZON, DECISION MAKING
 
-handle_ingest_use_case       = @ingest_use_case
-handle_set_time_horizon      = @set_time_horizon
+handle_ingest_use_case       = @ingest_use_case         ;
+handle_set_time_horizon      = @set_time_horizon        ;
 
 
 % ---------- FILE HANDLING
 
-handle_trim_timeseries_and_copy         = @trim_timeseries_and_copy
-handle_copy_files_to_current_simulation = @copy_files_to_current_simulation
+handle_trim_timeseries_and_copy         = @trim_timeseries_and_copy          ;
+handle_copy_files_to_current_simulation = @copy_files_to_current_simulation  ;
 
 cd ../
 
