40ccbcf3eb58d987511733c2ef68b6f64b7b158e
rjb2112 <robert.james.baker.2112@gmail.com>
2023-05-31 12:30:31 -0700

    Update params_simulation_environment.m

--- params_simulation_environment.m
+++ params_simulation_environment.m
@@ -3,6 +3,7 @@ warning('off', 'MATLAB:MKDIR:DirectoryExists');
 cd tools
 handle_interpolate_solar = @interpolate_solar
 handle_ingest_use_case   = @ingest_use_case
+handle_set_time_horizon  = @set_time_horizon
 cd ../
 
 
