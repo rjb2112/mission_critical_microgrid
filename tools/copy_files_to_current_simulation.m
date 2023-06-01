d0ad66271755dea6ffcccf12ce95d703063982ef
rjb2112 <robert.james.baker.2112@gmail.com>
2023-06-01 08:18:33 -0700

    l

--- tools/copy_files_to_current_simulation.m
+++ tools/copy_files_to_current_simulation.m
@@ -10,4 +10,14 @@ directory_destination = strcat("current_simulation");
 file_destination = "x_T_1s.csv" ;
 filename_destination = strcat(directory_destination,"/",file_destination) ;
 
+
+directory_source = strcat("use_cases/use_case_",use_case_id,"/",alternative_id);
+file_source = "x_Irr_1s.csv" ;
+filename_source = strcat(directory_source,"/",file_source) ;
+
+directory_destination = strcat("current_simulation");
+file_destination = "x_Irr_1s.csv" ;
+filename_destination = strcat(directory_destination,"/",file_destination) ;
+
+
 copyfile(filename_source, filename_destination) ;
