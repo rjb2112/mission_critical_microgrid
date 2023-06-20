13a81f79777cdbba4434ab0970b6abdb5a49070f
rjb2112 <robert.james.baker.2112@gmail.com>
2023-06-20 09:47:04 -0700

    ok

--- tools/copy_files_to_current_simulation.m
+++ tools/copy_files_to_current_simulation.m
@@ -2,20 +2,16 @@
 
 function copy_files_to_current_simulation(use_case_id, alternative_id)
 
-directory_source = strcat("use_cases/use_case_",use_case_id,"/",alternative_id);
+directory_source      = strcat("use_cases/use_case_",use_case_id,"/",alternative_id);
 directory_destination = strcat("current_simulation");
 
 Z = dir;
 
 for i=3:size(Z,1) % .. and . are two "files" in dir, start at 3
 file_source      = Z(i).name;
-file_destination = Z(i).name;
+file_destination = Z(i).name
 filename_source      = strcat(directory_source,"/",file_source) ;
 filename_destination = strcat(directory_destination,"/",file_destination) ;
 copyfile(filename_source, filename_destination) ;
 
 end
-
-
-
-copyfile(filename_source, filename_destination) ;
