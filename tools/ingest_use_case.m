3716f8518f32858bf14ab11da0fe5059ff2bb4b2
rjb2112 <robert.james.baker.2112@gmail.com>
2023-06-27 09:47:03 -0700

    ok

--- tools/ingest_use_case.m
+++ tools/ingest_use_case.m
@@ -1,17 +1,8 @@
-  tic;
-  fprintf("\n");
-  code_section_number = 200;
-  code_section_string = "ingest_use_case";
-  fprintf("%3d     %-40s",code_section_number,code_section_string);
-
-
-
 directory_source = strcat("use_cases/use_case_",use_case_id);
 file_source = "use_case_definition.csv";
 filename = strcat(directory_source,"/",file_source);
 
 fileid = fopen(filename);
-% fprintf("opening %s\nfor use case definition...\n",filename);
 
 X = textscan(fileid,'%s %s',...
   'Delimiter',',');
@@ -22,20 +13,47 @@ fclose(fileid);
 use_case_id_str        = string(X{2}{1});
 assert(use_case_id_str == use_case_id);
 
-time_horizon_start_str = string(X{2}{2});
-time_horizon_end_str   = string(X{2}{3});
+simulation_time_horizon_start_str = string(X{2}{2});
+simulation_time_horizon_end_str   = string(X{2}{3});
 
-% formatSpec = "yyyy-MM-dd HH:mm:ss";
-time_horizon_start_dt  = datetime(time_horizon_start_str,'InputFormat',datetimeFormatSpec);
-time_horizon_end_dt    = datetime(time_horizon_end_str,  'InputFormat',datetimeFormatSpec);
+decision_time_horizon_start_str = string(X{2}{4});
+decision_time_horizon_end_str   = string(X{2}{5});
+
+% values
+
+T.sim_t1  = datetime(simulation_time_horizon_start_str,'InputFormat',datetimeFormatSpec);
+T.sim_t2  = datetime(simulation_time_horizon_end_str,'InputFormat',datetimeFormatSpec);
+T.sim_tn  = seconds(T.sim_t1 - T.sim_t2) + 1   ;
+T.sim_t1s = string(T.sim_t1,datetimeFormatSpec);
+T.sim_t2s = string(T.sim_t2,datetimeFormatSpec);
+T.sim_tns = string(T.sim_tn);
+
+T.dec_t1  = datetime(decision_time_horizon_start_str,'InputFormat',datetimeFormatSpec);
+T.dec_t2  = datetime(decision_time_horizon_end_str,'InputFormat',datetimeFormatSpec);
+T.dec_tn  = seconds(T.sim_t1 - T.sim_t2) + 1   ;
+T.dec_t1s = string(T.dec_t1,datetimeFormatSpec);
+T.dec_t2s = string(T.dec_t2,datetimeFormatSpec);
+T.dec_tns = string(T.dec_tn);
 
 
-  fprintf("%.2f",toc);
 
-fprintf(" ━ USE CASE [ %s ]\n",use_case_id_str);
+% arrays
+
+T.sim_dt_array = transpose(T.sim_t1:seconds(1):T.sim_t2);
+T.sim_clock = transpose(1:1:T.sim_tn);
+
+T.dec_dt_array = transpose(T.dec_t1:seconds(1):T.dec_t2);
+T.dec_clock = transpose(1:1:T.dec_tn);
+
+
+fprintf(" ━ USE CASE [ %s ]\n",use_case_id);
 % fprintf("%s to ",time_horizon_start_str);
 % fprintf("%s\n",time_horizon_end_str);
 
-clear directory_source file_source fileid filename
+clear directory_source file_source fileid filename X
+clear simulation_time_horizon_start_str
+clear simulation_time_horizon_end_str
+clear decision_time_horizon_start_str
+clear decision_time_horizon_end_str
 
 
